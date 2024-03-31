0bf725a43cfc30242946cba699e28f2b96edd034
Zelltress <showofthesoul@gmail.com>
2024-03-31 14:13:12 +0700

    add 0.2

--- game_test.py
+++ game_test.py
@@ -1,10 +1,12 @@
+import text as tx
+
 if __name__ == "__main__":
   while True:
-    print("Welcome to the King Simulator!")
-    print("After the death of the last king, the throne passed to you.")
-    print("You are still young, you do not have the influence and authority of the previous ruler.")
-    print("You can choose to walk in multiple directions to find a way out.")
-    print("Let's start with your name: ")
-    name = input()
-    print("Good luck, " +name+ ".")
+    print(tx.GREETING)
+    print(tx.PLOT_1)
+    print(tx.PLOT_2)
+    print(tx.GM_DSCRPTN_1)
+    print(tx.GM_DSCRPTN_2)
+    print(tx.GM_DSCRPTN_3)
+    print(tx.START)
     introScene()
