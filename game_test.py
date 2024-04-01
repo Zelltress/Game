a0c4d61c974da48c425ac16843bbde64fb42fe57
ZellTress <showofthesoul@gmail.com>
2024-04-01 09:16:09 +0700

    add 0.4

--- game_test.py
+++ game_test.py
@@ -1,28 +1,25 @@
 import text as tx
 import random
 
-'''
-def introScene():
-    priority = ['user_1', 'user_2', 'user_3', 'user_4']
-    users = {}
 
-    print(tx.PRIORITY)
+def intro_scene():
+    priority = ['user_1', 'user_2', 'user_3', 'user_4']
+
+    print(tx.PRIORITY_1)
     user_1 = input(tx.USER)
     user_2 = input(tx.USER)
     user_3 = input(tx.USER)
     user_4 = input(tx.USER)
-'''
-priority = ['user_1', 'user_2', 'user_3', 'user_4']
-pr = [1, 2, 3, 4]
-users = {}
 
-random.shuffle(pr)
-print(pr)
-for i in range(1, 4):
-    priority[i] = priority[pr[i]]
-print(priority)
+    random.shuffle(priority)
 
-'''
+    print(tx.PRIORITY_2)
+
+def ounter_quarter():
+
+
+
+
 if __name__ == "__main__":
     while True:
         print(tx.GREETING)
@@ -32,5 +29,4 @@ if __name__ == "__main__":
         print(tx.GM_DSCRPTN_2)
         print(tx.GM_DSCRPTN_3)
         print(tx.START)
-        introScene()
-'''
+        intro_scene()
