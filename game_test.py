7cf81115468233625b4171f4145ce77656f4c08d
ZellTress <showofthesoul@gmail.com>
2024-04-01 10:33:02 +0700

    add 0.6

--- game_test.py
+++ game_test.py
@@ -1,6 +1,32 @@
 import text as tx
 import random
 
+mng_event = 0
+non_mng_event = 0
+plot_event = 0
+
+
+class user():
+
+    @classmethod
+    def __init__(self, chrch, army, ppl, wrld_authority):
+        self.chrch = chrch
+        self.army = army
+        self.ppl = ppl
+        self.wrld_authority = wrld_authority
+
+    @staticmethod
+    def сounter_quarter():
+        global mng_event, non_mng_event, plot_event
+        track = 0
+        count = 0
+        while track == user:
+            if mng_event == 1 and non_mng_event == 1 and plot_event == 1:
+                count += 1
+                mng_event = 0
+                non_mng_event = 0
+                plot_event = 0
+
 
 def intro_scene():
     priority = ['user_1', 'user_2', 'user_3', 'user_4']
@@ -13,32 +39,10 @@ def intro_scene():
 
     random.shuffle(priority)
 
+    random.randint(1,100)
+    priority =
+
     print(tx.PRIORITY_2)
-
-
-mng_event = 0
-non_mng_event = 0
-plot_event = 0
-
-
-class user():
-
-    def __init__(self, chrch, army, ppl):
-        self.chrch = chrch
-        self.army = army
-        self.ppl = ppl
-
-    def сounter_quarter(self):  # self ссылается на конкретный объект класса
-        global mng_event, non_mng_event, plot_event
-        track = 0
-        count = 0
-        while track == user:
-            if mng_event == 1 and non_mng_event == 1 and plot_event == 1:
-                count += 1
-                mng_event = 0
-                non_mng_event = 0
-                plot_event = 0
-
 
 
 if __name__ == "__main__":
