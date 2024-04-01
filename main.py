ec1ff285600dee972a044a34edd9abd19331befc
NastYouSha <a.ivanova18@g.nsu.ru>
2024-04-01 13:46:53 +0700

    Create window for game

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((600, 300))
+
+running = True
+while running:
+
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+
