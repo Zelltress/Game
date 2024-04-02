c8ba6671c9e6c9d1d5b9639bdfea1e3c9f57ced9
Zelltress <showofthesoul@gmail.com>
2024-04-02 20:59:27 +0700

    add 0.8.3

--- game_test.py
+++ game_test.py
@@ -4,30 +4,30 @@ import random
 
 class User:
 
-    # @classmethod
-    def __init__(self, name, chrch, army, ppl, wrld_authority):
+    def __init__(self, name, church, army, ppl, wrld_authority):
         self.name = name
-        self.chrch = chrch
+        self.church = church
         self.army = army
         self.ppl = ppl
         self.wrld_authority = wrld_authority
 
     def display_stat(self):
-        print(tx.STATISTIC.format(self.name, self.chrch, self.army, self.ppl))
+        print(tx.STATISTIC.format(self.name, self.church, self.army, self.ppl))
 
     def quarter(self):
         mng_event = 0
         non_mng_event = 0
         plot_event = 0
         count = 5
-        while self == User: #еще нужно проверить на работоспособность
+        while self == User:  # еще нужно проверить на работоспособность
             if mng_event == 1 and non_mng_event == 1 and plot_event == 1:
                 count += 1
                 mng_event = 0
                 non_mng_event = 0
                 plot_event = 0
 
-        print(tx.QUARTER, count*3)
+        print(tx.QUARTER, count * 3)
+        return count
 
 
 def intro_scene():
@@ -42,25 +42,26 @@ def intro_scene():
 
 
 if __name__ == "__main__":
-    # while True:
+    while User.quarter() <= 40:
+        intro_scene()
 
-    intro_scene()
+        user_name_1 = input(tx.USER)
+        user_name_2 = input(tx.USER)
+        user_name_3 = input(tx.USER)
+        user_name_4 = input(tx.USER)
 
-    user_name_1 = input(tx.USER)
-    user_name_2 = input(tx.USER)
-    user_name_3 = input(tx.USER)
-    user_name_4 = input(tx.USER)
+        priority = [user_name_1, user_name_2, user_name_3, user_name_4]
+        random.shuffle(priority)
 
-    priority = [user_name_1, user_name_2, user_name_3, user_name_4]
-    random.shuffle(priority)
+        print(tx.PRIORITY_2)
+        print(priority)
 
-    print(tx.PRIORITY_2)
-    print(priority)
+        user_1 = User(user_name_1, 20, 60, 45, 1)
+        user_2 = User(user_name_2, 20, 60, 45, 1)
+        user_3 = User(user_name_3, 20, 60, 45, 1)
+        user_4 = User(user_name_4, 20, 60, 45, 1)
 
-    user_1 = User(user_name_1, 20, 60, 45, 1)
-    user_2 = User(user_name_2, 20, 60, 45, 1)
-    user_3 = User(user_name_3, 20, 60, 45, 1)
-    user_4 = User(user_name_4, 20, 60, 45, 1)
+        user_1.display_stat()
+        user_1.quarter()
 
-    user_1.display_stat()
-    user_1.quarter()
+
