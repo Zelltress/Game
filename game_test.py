fcdad75f5b585eaa9b1c8a802a9f954ae4fd8514
ZellTress <showofthesoul@gmail.com>
2024-04-10 09:13:23 +0700

    add 0.8.7

--- game_test.py
+++ game_test.py
@@ -13,19 +13,9 @@ class User:
         self.ppl = ppl
         self.wrld_authority = wrld_authority
         self.treasury = treasury
-        #self.attrlist = ['church', 'army', 'ppl']
-
-    '''
-    def dependence(self):
-        rand_attr = random.choice(self.attrlist)
-        attr_value = getattr(self, rand_attr)
-        setattr(self, rand_attr, attr_value + 10)
-    '''
 
     def display_stat(self):
-        print('\n',tx.STATISTIC.format(self.name, self.church, self.army, self.ppl, self.treasury))
-
-    # def description(self):
+        print('\n', tx.STATISTIC.format(self.name, self.church, self.army, self.ppl, self.treasury))
 
 
 def intro_scene():
@@ -56,57 +46,57 @@ if __name__ == "__main__":
 
     while count < 10 or (user_1.army or user_1.ppl or user_1.church or user_1.treasury) != 0:
 
-        print('\n',user_name_1, "! ", '\n', tx.PLOT_3, sep='')
+        print('\n', user_name_1, "! ", '\n', tx.PLOT_3, sep='')
 
         print(tx.PEOPLE_SPRNG[0])
         user_wrd = input()
         i = random.randint(5, 15)
         j = random.randint(20, 50)
-        if user_wrd.lower() == answer[0].lower():
-            user_1.ppl = user_1.ppl + i
-            user_1.treasury = user_1.treasury - j
+        if user_wrd.lower() == answer[0]:
+            user_1.ppl += i
+            user_1.treasury -= j
         else:
-            user_1.ppl = user_1.ppl - i
+            user_1.ppl -= i
         user_1.display_stat()
 
         print('\n', tx.TREASURY_SPRNG[1])
         user_wrd = input()
         i = random.randint(5, 15)
         j = random.randint(20, 50)
-        if user_wrd.lower() == answer[0].lower():
-            user_1.ppl = user_1.ppl + i
-            user_1.treasury = user_1.treasury - j
+        if user_wrd.lower() == answer[0]:
+            user_1.ppl += i
+            user_1.treasury -= j
             user_1.wrld_authority = 2
         else:
-            user_1.ppl = user_1.ppl - i
+            user_1.ppl -= i
         user_1.display_stat()
-
 
         if user_1.wrld_authority == 2:
             print('\n', user_name_2, "! ", tx.PLOT_4[0], sep='')
         else:
             print('\n', user_name_2, "! ", tx.PLOT_4[1], sep='')
 
-
         print('\n', tx.ARMY_SPRNG[0])
         user_wrd = input()
         i = random.randint(5, 15)
         j = random.randint(20, 50)
-        if user_wrd.lower() == answer[0].lower():
-            user_2.army = user_2.army + i
-            user_2.treasury = user_2.treasury - j
+        if user_wrd.lower() == answer[0]:
+            user_2.army += i
+            user_2.treasury -= j
         else:
-            user_2.army = user_2.army - i
+            user_2.army -= i
         user_2.display_stat()
 
         print('\n', tx.CHUCRH_SPRNG[1])
         user_wrd = input()
         i = random.randint(5, 15)
         j = random.randint(20, 50)
-        if user_wrd.lower() == answer[0].lower():
-            user_2.church = user_2.church + i
+        if user_wrd.lower() == answer[0]:
+            user_2.church += i
+            user_2.ppl -= i
         else:
-            user_2.ppl = user_2.ppl - j
+            user_2.church -= i
+            user_2.ppl += i
         user_2.display_stat()
 
         count += 1
@@ -115,52 +105,137 @@ if __name__ == "__main__":
         user_wrd = input()
         i = random.randint(5, 15)
         j = random.randint(20, 50)
-        if user_wrd.lower() == answer[0].lower():
-            user_1.ppl = user_1.ppl + i
-            user_1.church = user_1.church + i
-            user_1.treasury = user_1.treasury - j
+        if user_wrd.lower() == answer[0]:
+            user_1.ppl += i
+            user_1.church += i
+            user_1.treasury -= j
         else:
-            user_1.ppl = user_1.ppl - i
-            user_1.church = user_1.church - i
+            user_1.ppl -= i
+            user_1.church -= i
         user_1.display_stat()
 
         print('\n', tx.ARMY_SMMR[0])
         user_wrd = input()
         i = random.randint(5, 15)
         j = random.randint(20, 50)
-        if user_wrd.lower() == answer[0].lower():
-            user_1.army = user_1.army + i
-            user_1.treasury = user_1.treasury - j
+        if user_wrd.lower() == answer[0]:
+            user_1.army += i
+            user_1.treasury -= j
         else:
-            user_1.army = user_1.army + i
+            user_1.army += i
         user_1.display_stat()
 
         print('\n', tx.TREASURY_SMMR[0])
         user_wrd = input()
         i = random.randint(5, 15)
         j = random.randint(20, 50)
-        if user_wrd.lower() == answer[0].lower():
-            user_2.treasury = user_2.treasury + j
+        if user_wrd.lower() == answer[0]:
+            user_2.treasury += j
         else:
-            user_2.treasury = user_2.treasury - j
+            user_2.treasury -= j
         user_2.display_stat()
 
         print('\n', tx.PEOPLE_SMMR[1])
         user_wrd = input()
         i = random.randint(5, 15)
         j = random.randint(20, 50)
-        if user_wrd.lower() == answer[0].lower():
-            user_2.ppl = user_2.ppl + i
-            user_2.treasury = user_2.treasury - j
+        if user_wrd.lower() == answer[0]:
+            user_2.ppl += i
+            user_2.treasury -= j
             user_2.wrld_authority = 2
         else:
-            user_2.ppl = user_2.ppl - j
+            user_2.ppl -= i
         user_2.display_stat()
 
         count += 1
 
+        print(tx.TREASURY_FALL[0])
+        user_wrd = input()
+        i = random.randint(5, 15)
+        j = random.randint(20, 50)
+        if user_wrd.lower() == answer[0]:
+            user_1.ppl += i
+            user_1.treasury -= j
+        else:
+            user_1.ppl -= i
+        user_1.display_stat()
 
+        print('\n', tx.CHURCH_FALL[0])
+        user_wrd = input()
+        i = random.randint(5, 15)
+        j = random.randint(20, 50)
+        if user_wrd.lower() == answer[0]:
+            user_1.church += i
+            user_1.treasury -= j
+            if user_1.wrld_authority == 2:
+                user_1.ppl += i
+        else:
+            user_1.church -= i
+        user_1.display_stat()
 
+        print('\n', tx.TREASURY_FALL[1])
+        user_wrd = input()
+        i = random.randint(5, 15)
+        j = random.randint(20, 50)
+        if user_wrd.lower() == answer[0]:
+            user_2.treasury += j
+        else:
+            user_2.treasury -= j
+        user_2.display_stat()
 
+        print('\n', tx.PEOPLE_FALL[0])
+        user_wrd = input()
+        i = random.randint(5, 15)
+        j = random.randint(20, 50)
+        if user_wrd.lower() == answer[0].lower():
+            user_2.ppl += i
+            user_2.treasury -= j
+        else:
+            user_2.ppl -= i
+        user_2.display_stat()
 
+        count += 1
 
+        if user_1.church <= 10:
+            print(tx.CHURCH_WNTR[1])
+            user_wrd = input()
+            i = random.randint(5, 15)
+            j = random.randint(20, 50)
+            if user_wrd.lower() == answer[0]:
+                user_1.ppl -= i
+                user_1.army -= i
+                user_1.treasury -= j
+                user_1.church += 2 * i
+            elif user_1.wrld_authority == 1:
+                print(tx.END[0])
+                break
+
+            else:
+                print(tx.END[1])
+                break
+            user_1.display_stat()
+
+        print('\n', tx.ARMY_WNTR[1])
+        user_wrd = input()
+        i = random.randint(5, 15)
+        j = random.randint(20, 50)
+        if user_wrd.lower() == answer[0]:
+            user_2.treasury += j
+            user_2.army += i
+        else:
+            user_2.army -= i
+        user_2.display_stat()
+
+        print('\n', tx.PEOPLE_WNTR[0])
+        user_wrd = input()
+        i = random.randint(5, 15)
+        j = random.randint(20, 50)
+        if user_wrd.lower() == answer[0].lower():
+            user_2.ppl -= 2 * i
+            user_2.treasury -= 2 * j
+            user_2.wrld_authority = 2
+        else:
+            user_2.ppl -= i
+        user_2.display_stat()
+
+        count += 1
